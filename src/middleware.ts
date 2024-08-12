0d08a0951ac4cf119c73376caa1c69bc2e933bf3
Andrea <andrea.moralesbarrantes@gmail.com>
2024-08-12 00:10:51 -0400

    regions

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,20 +18,21 @@ async function getRegionMap() {
     regionMapUpdated < Date.now() - 3600 * 1000
   ) {
     // Fetch regions from Medusa. We can't use the JS client here because middleware is running on Edge and the client needs a Node environment.
-    const { regions } = await fetch(`${BACKEND_URL}/store/regions`, {
-      next: {
-        revalidate: 3600,
-        tags: ["regions"],
-      },
-    }).then((res) => res.json())
+    // const { regions } = await fetch(`${BACKEND_URL}/store/regions`, {
+    //   next: {
+    //     revalidate: 3600,
+    //     tags: ["regions"],
+    //   },
+    // }).then((res) => res.json())
 
-    if (!regions) {
-      return regionsDefault
-     // notFound()
-    }
-
+    // if (!regions) {
+    //   return regionsDefault
+    //  // notFound()
+    // }
+    const regions = {...regionsDefault}
     // Create a map of country codes to regions.
-    regions.forEach((region: Region) => {
+    //@ts-ignore
+    regionsDefault.regions.forEach((region: Region) => {
       region.countries.forEach((c) => {
         regionMapCache.regionMap.set(c.iso_2, region)
       })
